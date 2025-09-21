b026c1bda44f760371e184221332bdfbbdd575ff
Tofiq36 <2025a6r005@mietjammu.in>
2025-09-21 12:39:00 +0530

    Create euclidean_distance.c

---
+++ euclidean_distance.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float x1, y1, x2, y2, distance;
+    scanf("%f%f%f%f", &x1, &y1, &x2, &y2);
+    float result = pow((x2 - x1), 2) + pow((y2 - y1), 2);
+    distance = sqrt(result);
+    printf("%.2f", distance);
+    return 0;
+}
