3e60e3de2e14f63885fd30a66946129f0e461d61
Tofiq36 <2025a6r005@mietjammu.in>
2025-09-21 12:36:48 +0530

    Create academic_performance_calculator.c

---
+++ academic_performance_calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int English, Mathematics, Science, Computer, History, total, marks_obtained;
+    total = 500;
+    scanf("%d%d%d%d%d", &English, &Mathematics, &Science, &Computer, &History);
+    marks_obtained = English + Mathematics + Science + Computer + History;
+    float average = marks_obtained / 5;
+    float Percentage = (marks_obtained * 100) / total;
+    printf("%d %.2f %.2f", marks_obtained, average, Percentage);
+    return 0;
+}
